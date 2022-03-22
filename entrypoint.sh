70cc02c52bf98b7f0b9b528972a0082df72356ae
Matteo Duranti <bozzo1983@gmail.com>
2022-03-22 14:33:57 +0100

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 cd /home/testsys/current_dir
 
-DIRECTORY="JMDCCommander"
+DIRECTORY="AMSDAQ"
 if [ ! -d "$DIRECTORY" ]
 then
     # Control will enter here if $DIRECTORY doesn't exist.
-    svn checkout https://svn.code.sf.net/p/jmdccommander/code/trunk JMDCCommander
-    cd JMDCCommander
+    svn checkout svn checkout https://svn.code.sf.net/p/amsdaq/code/DAQ/DAQ AMSDAQ
+    cd AMSDAQ
     make clean; make
 else
-    echo "Found an already existing JMDCCommander checkout. Using it"
-    cd JMDCCommander
+    echo "Found an already existing AMSDAQ checkout. Using it"
+    cd AMSDAQ
 fi
 echo ""
 
